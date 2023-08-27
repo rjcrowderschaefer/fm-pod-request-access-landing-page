6fed199bee0b61f49f04e54c8a41049f53a6d3ce
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-08-27 18:43:49 -0400

    Update email validation and error messaging display logic

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-const email = document.getElementById("email");
+const email = document.getElementById('email');
+const requiredError = document.getElementById('required-error');
+const emailError = document.getElementById('email-error');
+const validSubmission = document.getElementById('valid-submission');
 
-email.addEventListener("input", (event) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Oops! Please check your email");
+function validateEmail(val) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(val);
+}
+
+function hideAllErrors() {
+    requiredError.style.display = 'none';
+    emailError.style.display = 'none';
+    validSubmission.style.display = 'none';
+}
+
+function validate() {
+    hideAllErrors();
+    if (!email.value) {
+        requiredError.style.display = 'block';
+        email.style.marginBottom = '0px';
+    } else if (!validateEmail(email.value)) {
+        emailError.style.display = 'block';
+        email.style.marginBottom = '0px';
     } else {
-        email.setCustomValidity("");
+        validSubmission.style.display = 'block';
+        email.style.marginBottom = '15px';
     }
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  validate();  
 });
