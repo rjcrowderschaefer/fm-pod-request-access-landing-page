f6f4a52c077d779294b8c671f4b6803e17f9d4bb
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-08-28 23:35:46 -0400

    Update validation error messaging logic for mobile breakpoint

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const email = document.getElementById('email');
+const formContainer = document.getElementById('form');
 const requiredError = document.getElementById('required-error');
 const emailError = document.getElementById('email-error');
 const validSubmission = document.getElementById('valid-submission');
+const smallMediaQuery = window.matchMedia('(min-width: 767px');
+const mediumMediaQuery = window.matchMedia('min-width: 768px');
+// const largeMediaQuery = window.matchMedia('min-width: 1275px');
 
 function validateEmail(val) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -16,15 +20,46 @@ function hideAllErrors() {
 
 function validate() {
     hideAllErrors();
-    if (!email.value) {
-        requiredError.style.display = 'block';
-        email.style.marginBottom = '0px';
-    } else if (!validateEmail(email.value)) {
-        emailError.style.display = 'block';
-        email.style.marginBottom = '0px';
+    if (mediumMediaQuery.matches) {
+        if (!email.value) {
+            requiredError.style.display = 'block';
+            email.style.marginBottom = '0px';
+        } else if (!validateEmail(email.value)) {
+            emailError.style.display = 'block';
+            email.style.marginBottom = '0px';
+        } else {
+            validSubmission.style.display = 'block';
+            email.style.marginBottom = '8px';
+        }
+    // } else if (largeMediaQuery.matches) {
+    //     if (!email.value) {
+    //         requiredError.style.display = 'block';
+    //         email.style.marginBottom = '0px';
+    //     } else if (!validateEmail(email.value)) {
+    //         emailError.style.display = 'block';
+    //         email.style.marginBottom = '0px';
+    //     } else {
+    //         validSubmission.style.display = 'block';
+    //         email.style.marginBottom = '8px';
+    //     }
     } else {
-        validSubmission.style.display = 'block';
-        email.style.marginBottom = '8px';
+        if (!email.value) {
+            formContainer.style.position = 'relative';
+            requiredError.style.display = 'block';
+            requiredError.style.position = 'absolute';
+            requiredError.style.top = '40%'
+            email.style.marginBottom = '25px';
+        } else if (!validateEmail(email.value)) {
+            formContainer.style.position = 'relative';
+            emailError.style.display = 'block';
+            emailError.style.position = 'absolute';
+            emailError.style.top = '40%'
+            email.style.marginBottom = '25px';
+        } else {
+            validSubmission.style.display = 'block';
+            email.style.marginBottom = '15px';
+            validSubmission.style.marginTop = '10px';
+        }
     }
 }
 
@@ -32,3 +67,7 @@ form.addEventListener('submit', function(e) {
   e.preventDefault();
   validate();  
 });
+
+mediumMediaQuery.addEventListener('change', validate());
+
+// largeMediaQuery.addEventListener('change', validate);
